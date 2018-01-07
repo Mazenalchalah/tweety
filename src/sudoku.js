17c8406a64a772397864d455b42fe6743f78bf4e
esraahusamia <esraa.hus93@yahoo.com>
2018-01-07 23:30:10 +0200

    sdfs

--- src/sudoku.js
+++ src/sudoku.js
@@ -24,6 +24,8 @@ function sudoku(grid) {
                         return false;
                     }
                 }
+
+                
             }
             else {
                 return false;
